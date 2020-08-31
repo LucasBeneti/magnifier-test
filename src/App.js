3454b01a7139eb4e405aff2466e404ad327fb67b
Lucas Silva Beneti <LucasBeneti@Cesars-MacBook-Pro-2.local>
2020-08-31 18:43:35 -0300

    Still testing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <h1>Teste do Github pages</h1>
       <h1>É só commitar as mudança que já rola parece</h1>
+      <h1>Tenho que saber se tenho que dar o deploy em toda mudança</h1>
     </div>
   );
 }
