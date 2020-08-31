db004b58aed8fcde56f5c51157e5c33535691f8a
Lucas Silva Beneti <LucasBeneti@Cesars-MacBook-Pro-2.local>
2020-08-31 18:21:15 -0300

    Updates

---
+++ precache-manifest.60a5ce5a208f1d14b9996c28fda0d47b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b22db068ac8d9c03394781b3e7362a2",
+    "url": "/magnifier-test/index.html"
+  },
+  {
+    "revision": "d50c1349c2eb3d0a287c",
+    "url": "/magnifier-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9615d97bd76e1c72387",
+    "url": "/magnifier-test/static/js/2.175ef762.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magnifier-test/static/js/2.175ef762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d50c1349c2eb3d0a287c",
+    "url": "/magnifier-test/static/js/main.b8ec600c.chunk.js"
+  },
+  {
+    "revision": "4dec6584d03f3d2f4bac",
+    "url": "/magnifier-test/static/js/runtime-main.adc4ae2e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/magnifier-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
