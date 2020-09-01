ee8417c3816507bb92bd7e614074e713dd3441c4
lbeneti <lucasbeneti@alunos.utfpr.edu.br>
2020-09-01 17:39:27 -0300

    Mínimo feito

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React from "react";
-import logo from "./logo.svg";
+
+import ReactImageMagnify from "react-image-magnify";
+import InnerImageZoom from "react-inner-image-zoom";
+
+import img352 from "./assets/img352.png";
+import img1760 from "./assets/img1760.jpg";
+
 import "./App.css";
+import "./innerImageStyles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Teste do Github pages</h1>
-      <h1>É só commitar as mudança que já rola parece</h1>
-      <h1>Tenho que saber se tenho que dar o deploy em toda mudança</h1>
-      <h1>Tenho que saber se tenho que dar o deploy em toda mudança</h1>
+      <div className="mainGrid">
+        <div className="imageMagnify" style={{ flexDirection: "column" }}>
+          <span className="zoomOptionTitle">Primeiro tipo</span>
+          <ReactImageMagnify
+            {...{
+              smallImage: {
+                alt: "Imagem menor",
+                isFluidWidth: true,
+                src: img352,
+              },
+              largeImage: {
+                src: img1760,
+                width: 1200,
+                height: 1900,
+              },
+            }}
+          />
+        </div>
+        <div className="imageZoom">
+          <span className="zoomOptionTitle">Segunda tipo</span>
+          <InnerImageZoom src={img352} zoomSrc={img1760} />
+        </div>
+      </div>
     </div>
   );
 }
